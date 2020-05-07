5f3a3ed5c08bf52d713c5056776e0ea651674ca9
Alex Bautista <lex.magno@gmail.com>
2020-05-07 04:15:59 -0500

    first commit

---
+++ src/app/login/register.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./login.component.css'], // asignamos css por ruta => styleUrls:
+})
+export class RegisterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
