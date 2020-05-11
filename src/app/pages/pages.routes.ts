8bc699d1c0e3ecc3bd7130cbe8737bed2c7d6770
Alex Bautista <lex.magno@gmail.com>
2020-05-11 04:10:55 -0500

    FIX: Rutas Login & Register

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,7 +4,6 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { NopagefoundComponent } from '../shared/nopagefound/nopagefound.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const pagesRoutes: Routes = [
