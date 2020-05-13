5a6fbe29387c53e169084d192a9f07b800e99672
Alex Bautista <lex.magno@gmail.com>
2020-05-13 15:22:43 -0500

    FIXED: Plantilla Dashboard Component

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,19 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `
-    <p>
-      dashboard works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './dashboard.component.html',
+  styles: [],
 })
 export class DashboardComponent implements OnInit {
+  graficos: any = {
+    grafico1: {
+      labels: ['Totales', 'Este Mes', 'Esta Semana'],
+      data: [37, 37, 11],
+      type: 'doughnut',
+      leyenda: 'Grafico por Mes',
+    },
+    grafico2: {
+      labels: ['Totales', 'al Service'],
+      data: [37, 16],
+      type: 'doughnut',
+      leyenda: 'Usuarios Consultados',
+    },
+    grafico3: {
+      labels: ['Almacenado', 'Service'],
+      data: [95, 5],
+      type: 'doughnut',
+      leyenda: 'Consultas del Aplicativo',
+    },
+    grafico4: {
+      labels: ['Bajo', 'Alto'],
+      data: [85, 15],
+      type: 'doughnut',
+      leyenda: 'Consumo de recursos',
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
